7151b77a62155b5e43be46a0425b9e4af0d065cf
clover <clover@solidyear.com.tw>
2016-11-22 15:56:22 +0800

    lifecycle hook: OnInit, OnDestroy, AfterView

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,35 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SpyDirective } from './spy.directive';
+import { SpyParentComponent }   from './spy.component';
+import { OnChangesComponent, OnChangesParentComponent } from './on-changes.component';
+import {
+    AfterViewParentComponent,
+    AfterViewComponent,
+    ChildViewComponent
+} from './after-view.component';
+
+// service
+import { LoggerService }    from './logger.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpyDirective,
+    SpyParentComponent,
+    OnChangesComponent,
+    OnChangesParentComponent,
+    AfterViewParentComponent,
+    AfterViewComponent,
+    ChildViewComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [LoggerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
